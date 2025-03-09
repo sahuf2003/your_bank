9d17ae5e48957d0fdf5f4a429fcf0e8a8f7328e1
Sahuf Shaikh <127684377+sahuf2003@users.noreply.github.com>
2025-03-09 19:44:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    historyApiFallback: true,  // This ensures that all routes fallback to index.html
+    historyApiFallback: true,  
   },
 });
